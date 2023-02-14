d8c368e92a14f0357c2fca782cdeac837987ec28
fjswnls <fjswnlss3@gmail.com>
2023-02-14 11:48:00 +0900

    무한루프 내에서 명령어 받기

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
 
-public class Main {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Main {
+    public static void main(String[] args) {
+		System.out.println("==프로그램 시작==");
+		
+		Scanner sc =new Scanner(System.in);
+		
+		System.out.println("명령어 ) ");
+		
+		String cmd =sc.nextLine();
+		
+		System.out.printf("입력된 명령어 : %s\n",cmd);
+		
+		System.out.println("==프로그램 끝==");
+		
+		sc.close();
 
 	}
 
